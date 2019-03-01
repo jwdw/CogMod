cdfff580f7fe09391a649f006cbcb9b5f703c7cd
J.W. de Wit <s2616602@ai178133.ai.rug.nl>
2019-03-01 10:20:41 +0100

    add files

---
+++ Hostage/Hostage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hostage
+//
+//  Created by J.W. de Wit on 01/03/2019.
+//  Copyright © 2019 J.W. de Wit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var Text: UITextView!
+    @IBAction func Test(_ sender: Any) {
+        print("hello")
+        Text.text = "fasdfsdf"
+    }
+    
+}
+
---
+++ Hostage/HostageTests/HostageTests.swift
@@ -0,0 +1,34 @@
+//
+//  HostageTests.swift
+//  HostageTests
+//
+//  Created by J.W. de Wit on 01/03/2019.
+//  Copyright © 2019 J.W. de Wit. All rights reserved.
+//
+
+import XCTest
+@testable import Hostage
+
+class HostageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
