8f4c79e9ca10efb202ddfcfed07d2e75a4ff12ac
S. Steffen <s3846504@ai178142.ai.rug.nl>
2019-03-28 16:09:16 +0100

    Theres some function to save and load something, but we cannot figure out where the f*** it saves it.

--- Hostage/Hostage/Game.swift
+++ Hostage/Hostage/Game.swift
@@ -269,12 +269,41 @@ class Game {
     }
     
     func teachAI() {
+        
         for chunk in currentChunks {
             print("chunky")
             chunk.setSlot(slot: "score", value: String(endScore))
             model.dm.addToDM(chunk)
         }
         model.time += 1
+        
+        
+        let fileName = "Test"
+        let dir = try? FileManager.default.url(for: .documentDirectory,
+                                               in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        // If the directory was found, we write a file to it and read it back
+        if let fileURL = dir?.appendingPathComponent(fileName).appendingPathExtension("txt") {
+            
+            // Write to the file named Test
+            let outString = "Write this text to the file"
+            do {
+                try outString.write(to: fileURL, atomically: true, encoding: .utf8)
+            } catch {
+                print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
+            }
+
+            // Then reading it back from the file
+            var inString = ""
+            do {
+                inString = try String(contentsOf: fileURL)
+            } catch {
+                print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
+            }
+            print("Read from the file: \(inString)")
+        }
+    
+        
     }
     
     func gameEnd() {
